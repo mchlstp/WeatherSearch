9ccebe5de368d30c17f3941b93be0b4f271343b2
mchlstp <michael.h.stump@gmail.com>
2021-03-09 15:29:22 -0600

    initial commit

---
+++ src/main/java/com/tts/WeatherApp/WeatherController.java
@@ -0,0 +1,35 @@
+package com.tts.WeatherApp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class WeatherController {
+    @Autowired
+    private WeatherService weatherService;
+
+	public WeatherService getWeatherService() {
+		return weatherService;
+	}
+
+	public void setWeatherService(WeatherService weatherService) {
+		this.weatherService = weatherService;
+	}
+	
+	@GetMapping
+	public String getIndex(Model model) {
+	    model.addAttribute("request", new Request());  
+	    return "index";
+	}
+	
+	@PostMapping
+	public String postIndex(Request request, Model model) {
+	    Response data = weatherService.getForecast(request.getZipCode());
+	    model.addAttribute("data", data);
+	    return "index";
+	}
+	
+}
